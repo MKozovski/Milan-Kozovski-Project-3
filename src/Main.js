8374e67a5cfa4d7135737ee7e69431259fd85922
Milan Kozovski <125383479+MKozovski@users.noreply.github.com>
2023-05-17 15:17:23 -0400

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -58,7 +58,7 @@ const Main = () => {
                     setCombinedResults(combinedResponse);
                 })
                 .catch((error) => {
-                    console.log(error);
+
                 });
         }
     };
